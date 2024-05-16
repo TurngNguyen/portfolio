07733f8929802fba2c41ebe0f5542b8a8b9b99d4
Trung Nguyen <89166770+TurngNguyen@users.noreply.github.com>
2024-05-16 04:30:48 -0700

    Added navbar, chest animation, sample card display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,168 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import About from './components/About/About';
+import Portfolio from './components/Portfolio/Portfolio';
+import React, { useState } from 'react';
+import { Box, Card, CardActions, CardActionArea, CardContent, Container, Fade, Typography, CardMedia } from '@mui/material';
+import minecraft_chest from './images/minecraft_chest.png';
+import minecraft_chest_open from './images/minecraft_chest_open.webp';
+import firework from './images/firework.webp';
+import magic_cards from './images/magic_cards.jpg';
 
 function App() {
+  const [clickCount, setClickCount] = useState(0);
+  const [rotate, setRotate] = useState(0);
+  const [showCard, setShowCard] = useState(false);
+
+  const handleClick = () => {
+    if (clickCount < 4) {
+      setClickCount(clickCount + 1);
+      setRotate(1);
+    } else if (clickCount === 4) {
+      setClickCount(clickCount + 1);
+      try {
+        openChest();
+        spawnFirework();
+        moveChest();
+      } catch (error) {
+        console.error(error);
+      }
+    } else { return }
+  };
+  
+  const openChest = async () => {
+    const chest = document.getElementById('chest');
+    chest.innerHTML = `<img src=${minecraft_chest_open} alt='minecraft chest open' />`;
+
+    await new Promise(resolve => setTimeout(resolve, 500));
+  };
+  
+  const spawnFirework = () => {
+    const fireworkElement = document.createElement('img');
+    fireworkElement.src = firework;
+    fireworkElement.style.width = '120px';
+    fireworkElement.style.height = '120px';
+    fireworkElement.style.position = 'absolute';
+
+    const animation = fireworkElement.animate(
+      [
+      { transform: 'translateY(0)' },
+      { transform: `translateY(-${window.innerHeight-fireworkElement.height}px)` }
+      ],
+      {
+      duration: 1000,
+      easing: 'linear',
+      fill: 'forwards'
+      }
+    );
+
+    const centeringDiv = document.getElementById('centerDiv');
+    centeringDiv.appendChild(fireworkElement);
+
+    animation.onfinish = () => {
+      centeringDiv.removeChild(fireworkElement);
+    };
+  };
+
+  const moveChest = () => {
+    const chest = document.getElementById('chest');
+    const animation = chest.animate(
+      [
+        { transform: 'translateY(0)' },
+        { transform: 'translateY(10vh)' }
+      ],
+      {
+        duration: 1000,
+        easing: 'linear',
+        fill: 'forwards'
+      }
+    );
+
+    animation.onfinish = () => {
+      closeChest();
+      setShowCard(true);
+    };
+  }
+
+  const closeChest = () => {
+    const chest = document.getElementById('chest');
+    chest.innerHTML = `<img src=${minecraft_chest} alt='minecraft chest' />`;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and  to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={
+            <>
+              <Container sx={{ 
+                height: '60vh', 
+                display: 'flex', 
+                justifyContent: 'center', 
+                alignItems: 'center',
+                }}>
+                <Fade in={showCard}>
+                  <Card sx={{ 
+                    height: '35vh', 
+                    backgroundColor: '#4e4e4e', 
+                    color: '#ffffff', 
+                    display: 'grid', 
+                    gridTemplateRows: '1fr 1fr' 
+                  }}>
+                    <CardMedia
+                      component="img"
+                      sx={{ height: '200px' }}
+                      image={magic_cards}
+                      alt='magic cards'
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h4" component="div">
+                        Magic the Gathering Card Indentifier
+                      </Typography>
+                      <Typography variant="body1" color="#f5f5f5" component="div">
+                        <ul style={{ listStyleType: 'none', paddingInlineStart: '0' }}>
+                          <li>Utilized urllib to download JSON card data and get card images via Scryfall API</li>
+                          <li>Visualized training and resulting data using matplotlib to train and improve model</li>
+                          <li>Learned Tensorflow to create Artificial Neural Network machine learning model to identify cards</li>
+                        </ul>
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Fade>
+              </Container>
+              <div id='centerDiv' style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'flex-end',
+              }}>
+                <Box
+                  id='chest'
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    cursor: 'pointer',
+                    position: 'absolute',
+                  }}
+                  rotate={rotate}
+                  onClick={handleClick}
+                  onAnimationEnd={() => setRotate(0)}
+                >
+                  <img src={minecraft_chest} alt='minecraft chest' />
+                </Box>
+              </div>
+            </>
+          }>
+          </Route>
+          <Route exact path="/portfolio" element={<Portfolio />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,154 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import { Link } from 'react-router-dom';
+
+const pages = ['Portfolio', 'About'];
+
+function NavBar() {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+    };
+
+
+
+const renderNameBigScreen = () => {
+    return (
+        <Typography
+            variant="h6"
+            noWrap
+            component={Link}
+            to="/"
+            sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+            }}
+        >
+            Trung Nguyen
+        </Typography>
+    );
+};
+
+const renderNavMenu = () => {
+    return (
+        // Box is a layout component that can be used to create a flexbox container
+        // Three line box that only shows on small screen sizes
+        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+            >
+                {/* The image of the three line box */}
+                <MenuIcon />
+            </IconButton>
+            {/* Menu opened by the three line box */}
+            <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                    display: { xs: 'block', md: 'none' },
+                }}
+            >
+                {/* Contents of menu */}
+                {pages.map((page) => (
+                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                        <Typography textAlign="center">{page}</Typography>
+                    </MenuItem>
+                ))}
+            </Menu>
+        </Box>
+    );
+};
+
+const renderNameSmallScreen = () => {
+    return (
+        <Typography
+            variant="h5"
+            noWrap
+            component={Link}
+            to="/"
+            sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+            }}
+        >
+            Trung Nguyen
+        </Typography>
+    );
+};
+
+const renderNavLinks = () => {
+    return (
+        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+                <Button
+                    key={page}
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: 'white', display: 'block' }}
+                    component={Link}
+                    to={`/${page.toLowerCase()}`}
+                >
+                    {page}
+                </Button>
+            ))}
+        </Box>
+    );
+};
+
+
+return (
+    <AppBar position="static" sx={{ backgroundColor: '#353535' }}>
+        <Container maxWidth="xl">
+            <Toolbar disableGutters>
+                {/* Renders these two on larger screen sizes */}
+                {renderNameBigScreen()}
+                {renderNavLinks()}
+                {/* Only renders these two on smaller screen sizes */}
+                {renderNavMenu()}
+                {renderNameSmallScreen()}
+            </Toolbar>
+        </Container>
+    </AppBar>
+);
+}
+export default NavBar;
